7f9ad388b14a667d5d11c55c42b6fdb6c5d41b99
Nicola Comand <nicmen89@gmail.com>
2012-03-24 19:33:05 +0000

    nuova servlet con riferimento a new_location_landing.jsp da creare

---
+++ test/src/Servlet/Inserimento_localita.java
@@ -0,0 +1,43 @@
+package Servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import Entita.*;
+import Utils.*;
+import Store.*;
+
+/**
+ *
+ */
+public class Inserimento_localita extends HttpServlet {
+
+	/**
+	 * Gestisce le richieste HTTP GETT
+	 * 
+	 * @param req
+	 *            la richiesta ricevuta dal client.
+	 * @param res
+	 *            la rispota fornita dal server.
+	 * 
+	 * @throws ServletException
+	 *             se ci sono problemi nell'esecuzione della servlet.
+	 * @throws IOException
+	 *             se ci sono problemi nella comunicazione client-server.
+	 */
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		String page = "";
+		Localita loc = new Localita(req.getParameter("city"),req.getParameter("province"));
+		loc = StoreLocalita.insertLocalita(loc);
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("stazione_nuova",loc); //passo il costrutto parametri alla pagina
+		page = "jsp/new_location_landing.jsp"; 
+			
+		res.sendRedirect(page);
+	}
+}
