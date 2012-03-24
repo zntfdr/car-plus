5bc4a7094be9c503a635164f33d6bc1baf06dd58
Francesco Agnolazza <crash.pc89@gmail.com>
2012-03-24 18:34:33 +0000

    aggiunto metodo readAbbonamento_List

--- test/src/Store/StoreAbbonamento.java
+++ test/src/Store/StoreAbbonamento.java
@@ -1,6 +1,9 @@
 package Store;
 import java.sql.*;
+import java.util.ArrayList;
+
 import Entita.Abbonamento;
+import Entita.Tragitto_CP;
 import Utils.*;
 
 public class StoreAbbonamento{
@@ -19,6 +22,26 @@ public class StoreAbbonamento{
 				while(rs.next()){
 					return new Abbonamento(rs.getString("nome"), rs.getString("descrizione"), rs.getDouble("tariffa_magg_100km"), rs.getDouble("tariffa_min_100km"), rs.getDouble("tariffa_notturna"), rs.getDouble("tariffa_diurna"), rs.getDouble("costo_mensile"), rs.getInt("num_max_tessere"), rs.getInt("num_min_tessere"));
 				}
+			}catch (SQLException e){
+			}
+		}
+		return null;
+	}
+	
+	public static ArrayList<Abbonamento> readAbbonamento_List(ArrayList<String> listaAbbonamenti){
+		String sql = "SELECT * FROM abbonamento WHERE ";
+		for (int i=0; i<listaAbbonamenti.size() - 1; i++){
+			sql += "nome = '" + listaAbbonamenti.get(i)+ "' OR ";
+		}
+		sql+= "nome = '" + listaAbbonamenti.get(listaAbbonamenti.size() - 1)+ "'";
+		ResultSet rs = Query.doQueryRS(sql);
+		if(rs != null){
+			try {
+				ArrayList<Abbonamento> Abbonamenti_List = new ArrayList<Abbonamento>();
+				while(rs.next()){
+					Abbonamenti_List.add(new Abbonamento (rs.getString("nome"), rs.getString("descrizione"), rs.getDouble("tariffa_magg_100km"), rs.getDouble("tariffa_min_100km"), rs.getDouble("tariffa_notturna"), rs.getDouble("tariffa_diurna"), rs.getDouble("costo_mensile"), rs.getInt("num_max_tessere"), rs.getInt("num_min_tessere")));
+				}
+				return Abbonamenti_List;
 			}catch (SQLException e){
 			}
 		}
