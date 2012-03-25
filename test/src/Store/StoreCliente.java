fd86c0229a4029bca6838de3d6d8cdfea047b5c9
Gianmarco Nalin <gianma.nalin89@gmail.com>
2012-03-25 20:38:51 +0000

    Store per Cliente (normale)

---
+++ test/src/Store/StoreCliente.java
@@ -0,0 +1,28 @@
+package Store;
+import java.sql.*;
+import Entita.Cliente;
+import Utils.*;
+
+public class StoreCliente {
+	public Cliente insertCliente(Cliente c) {
+		String sql_query = "INSERT INTO cliente (email, codice_fiscale) VALUES ('" + c.getEmail_utente() + "', '" + c.getCodice_fiscale() + "')";
+		Query.doQuery(sql_query);
+		
+		return readCliente(c.getEmail_utente(), c.getCodice_fiscale());
+	}
+	
+	public Cliente readCliente(String email, String codice_fiscale){
+		String sql_query = "SELECT * FROM cliente WHERE email = '" + email + "'";
+		ResultSet rs = Query.doQueryRS(sql_query);
+		if (rs != null) {
+			try {
+				while (rs.next()) {
+					return new Cliente (rs.getString("email_utente"), rs.getString("cf"));
+				}
+			} catch (SQLException e) {
+				
+			}
+		}
+		return null;
+	}
+}
