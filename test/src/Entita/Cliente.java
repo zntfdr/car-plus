71505efd8bd39e6c067f30962cea817db94e4c7e
Gianmarco Nalin <gianma.nalin89@gmail.com>
2012-03-25 20:05:23 +0000

    Entità che rappresenta un cliente non business Forse ha senso estendere Cliente_business da Cliente

---
+++ test/src/Entita/Cliente.java
@@ -0,0 +1,32 @@
+package Entita;
+
+public class Cliente {
+	private String email_utente;
+	private String codice_fiscale;
+	
+	public Cliente(String email_utente, String codice_fiscale) {
+		this.email_utente = email_utente;
+		this.codice_fiscale = codice_fiscale;
+	}
+	
+	public Cliente(){
+		email_utente = codice_fiscale = "";
+	}
+	
+	public void setEmail_utente(String newValue) {
+        email_utente = newValue;
+    }
+	
+	public void setCodice_fiscale(String newValue){
+		codice_fiscale = newValue;
+	}
+	
+	public String getEmail_utente() {
+		return email_utente;
+	}
+	
+	public String getCodice_fiscale(){
+		return codice_fiscale;
+	}
+
+}
