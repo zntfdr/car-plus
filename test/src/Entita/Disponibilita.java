e849d477e002139dec4843885c49775f4cc3a2ad
Nicola Comand <nicmen89@gmail.com>
2012-03-27 11:28:16 +0000

    nuova classe disponibilita

---
+++ test/src/Entita/Disponibilita.java
@@ -0,0 +1,36 @@
+package Entita;
+import java.sql.Timestamp;
+
+public class Disponibilita {
+	public int modello_macchina;
+    public int nome_abbonamento;
+
+	public Disponibilita(int newmod, int newna)
+	{
+		modello_macchina = newmod;
+		nome_abbonamento = newna;
+	}
+	
+	public void setModello(int mod)
+	{
+		modello_macchina = mod;
+		
+	}
+	
+	public void setNome_abb(int na)
+	{
+		nome_abbonamento = na;
+		
+	}
+
+	public int getModello() {
+		return modello_macchina;
+	}
+	
+	public int getNome_abb() {
+		return nome_abbonamento;
+	}
+
+	
+
+}
