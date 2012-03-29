61a1b881bbd1858ca73966c8c9e3f7c38a667f61
Francesco Agnolazza <crash.pc89@gmail.com>
2012-03-29 09:03:49 +0000

    Sistemato date di scadenza

--- test/src/Servlet/Inserimento_macchina_cs.java
+++ test/src/Servlet/Inserimento_macchina_cs.java
@@ -1,18 +1,13 @@
 package Servlet;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 import Entita.*;
 import Utils.*;
 import Store.*;
@@ -37,33 +32,14 @@ public class Inserimento_macchina_cs extends HttpServlet {
 	 */
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
-		//le prossime dichiarazioni mi servono per convertire le date
-		SimpleDateFormat formato_DB = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-		java.util.Date temp;
-
-		Timestamp scadenza_bollo = null;
-		Timestamp scadenza_assicurazione = null;
-		Timestamp scadenza_revisione = null;
-
-		try { 
-			temp = formato_DB.parse(req.getParameter("scadenza_bollo"));
-			scadenza_bollo = new Timestamp(temp.getTime());
-		} catch (ParseException e) { e.printStackTrace(); }
-
-		try { 
-			temp = formato_DB.parse(req.getParameter("scadenza_assicurazione"));
-			scadenza_bollo = new Timestamp(temp.getTime());
-		} catch (ParseException e) { e.printStackTrace(); }
-
-		try { 
-			temp = formato_DB.parse(req.getParameter("scadenza_revisione"));
-			scadenza_bollo = new Timestamp(temp.getTime());
-		} catch (ParseException e) { e.printStackTrace(); }
-
-		//Fine conversione date
-
+		
+		String dataTest = req.getParameter("scadenza_bollo");
+		Calendar scadenza_bollo = Utils.TimeString.dataStringToCalendar(req.getParameter("scadenza_bollo"));
+		Calendar scadenza_assicurazione = Utils.TimeString.dataStringToCalendar(req.getParameter("scadenza_assicurazione"));
+		Calendar scadenza_revisione = Utils.TimeString.dataStringToCalendar(req.getParameter("scadenza_revisione"));
+		String dataTest2 = Utils.TimeString.dataCalendarToString(scadenza_bollo);
 		//Inserisco Macchina car sharing
-		Macchina_CS mCS = new Macchina_CS(req.getParameter("car_plate"),Integer.parseInt(req.getParameter("model")),req.getParameter("city"),req.getParameter("province"), req.getParameter("name_cs"),Integer.parseInt(req.getParameter("purchase_year")),scadenza_bollo,scadenza_bollo,scadenza_bollo,Integer.parseInt(req.getParameter("tot_km")),Boolean.parseBoolean("TRUE"));		
+		Macchina_CS mCS = new Macchina_CS(req.getParameter("car_plate"),Integer.parseInt(req.getParameter("model")),req.getParameter("city"),req.getParameter("province"), req.getParameter("name_cs"),Integer.parseInt(req.getParameter("purchase_year")),scadenza_bollo, scadenza_assicurazione, scadenza_revisione, Integer.parseInt(req.getParameter("tot_km")),Boolean.parseBoolean("TRUE"));		
 		mCS =  StoreMacchina_CS.insertMacchina_CS(mCS);
 
 		//Se tutto va bene, comincio a definire la sessione
@@ -86,6 +62,9 @@ public class Inserimento_macchina_cs extends HttpServlet {
 			}
 		}
 		session.setAttribute("macchina_nuova",mCS); //passo il costrutto parametri alla pagina
+		session.setAttribute("dataTest", dataTest);
+		session.setAttribute("dataTest2", dataTest2);
+
 		page = "jsp/new_car_cs_landing.jsp";	
 
 		res.sendRedirect(page);
