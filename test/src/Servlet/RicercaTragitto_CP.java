dfba064aa75fe6024f94b4cbb8984df4c9da0a51
Francesco Agnolazza <crash.pc89@gmail.com>
2012-03-29 19:59:22 +0000

    Sistemato ricerca

--- test/src/Servlet/RicercaTragitto_CP.java
+++ test/src/Servlet/RicercaTragitto_CP.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Calendar;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import Entita.*;
@@ -17,34 +19,35 @@ public class RicercaTragitto_CP extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 
-
+		boolean fumatori = false;
 		String partenza = req.getParameter("from");
 		String arrivo = req.getParameter("to");
-		Timestamp data = new Timestamp(2012, 01, 14, 10, 11, 22, 11);
-		//data.valueOf(req.getParameter("date"));
+		Calendar data = Utils.TimeString.dataStringToCalendar(req.getParameter("date"));
 		int num_posti = Integer.parseInt(req.getParameter("people"));
-		//boolean fumatori = Boolean.parseBoolean(req.getParameter("smoker"));
-		boolean fumatori = false;
-		String result = "/jsp/searchResult.jsp";
-
-		String sql ="SELECT T.id, Lp.citta AS partenza, La.citta AS arrivo FROM tragitto_CP AS T INNER JOIN localita AS Lp ON T.id_localita_partenza = Lp.id INNER JOIN localita AS La ON T.id_localita_arrivo = La.id WHERE Lp.citta = '"+ partenza +"' AND La.citta = '"+ arrivo + "'";
-
-		ArrayList<Tragitto_CP> listaTragitti_CP = new ArrayList<Tragitto_CP>();
-		ResultSet rs = Query.doQueryRS(sql);
-		try {
-			while(rs.next()){
-				Tragitto_CP tmp = StoreTragitto_CP.readTragitto_CP(rs.getInt("id"));
-				tmp.setLocalita_partenza(rs.getString("partenza"));
-				tmp.setLocalita_arrivo(rs.getString("arrivo"));
-				listaTragitti_CP.add(tmp);
-			}
-		}catch (SQLException e){
+		if(req.getParameter("smoker")!=null){
+			fumatori = true;
 		}
 
 		HttpSession session = req.getSession();
-		session.setAttribute("listaTragitti_CP", listaTragitti_CP);
+		String result = "jsp/searchResult.jsp";
 
-		RequestDispatcher dispatcher = req.getRequestDispatcher(result);
-		dispatcher.forward(req, res);
+		String sql ="SELECT id FROM tragitto_CP WHERE citta_partenza = '"+ partenza +"' AND citta_arrivo = '"+ arrivo + "' AND tempo_partenza >= '" + Utils.TimeString.dataCalendarToString(data)+"' AND fumatori = '"+ fumatori + "' AND num_posti >= '" + num_posti + "'";
+		ResultSet rs = Query.doQueryRS(sql);
+
+		if(rs != null){
+			try {
+				ArrayList<Integer> listaIDTragitto = new ArrayList<Integer>();
+				while(rs.next()){
+					listaIDTragitto.add(rs.getInt("id"));
+				}
+				if(!listaIDTragitto.isEmpty()){
+					ArrayList<Tragitto_CP> listaTragitti_CP = Store.StoreTragitto_CP.readTragitto_CP_List(listaIDTragitto);
+					session.setAttribute("listaTragitti_CP", listaTragitti_CP);
+				} else result ="jsp/failure.jsp";
+			}catch (SQLException e){
+			}
+		} else result ="jsp/failure.jsp";
+		session.setAttribute("num_posti", num_posti);
+		res.sendRedirect(result);
 	}
 }
