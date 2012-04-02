b56f84b9cdd99ea6270f5f6f776b49feb157fe0a
Francesco Agnolazza <crash.pc89@gmail.com>
2012-04-02 16:48:22 +0000

    aggiunto metodo getTesseraFromContratto

--- test/src/Store/StoreTessera.java
+++ test/src/Store/StoreTessera.java
@@ -1,18 +1,21 @@
 package Store;
 import java.sql.*;
+import java.util.ArrayList;
+
 import Entita.Tessera;
+import Entita.Tragitto_CP;
 import Utils.*;
 
 public class StoreTessera{
     public static Tessera insertTessera(Tessera tess){
-        String sql = "INSERT INTO tessera(id, id_contratto) VALUES(" + tess.getId() + "," + tess.getId_contratto() + ")";
+        String sql = "INSERT INTO tessera(id, id_contratto) VALUES(" + tess.getId() + "," + tess.getContratto() + ")";
 
       Query.doQuery(sql);
-      return readTessera(tess.getId(),tess.getId_contratto());
+      return readTessera(tess.getId());
     }
 
-    public static Tessera readTessera(int id, int idc){
-        String sql = "SELECT * FROM tessera WHERE id = " + id + " AND id_contratto = "+ idc;
+    public static Tessera readTessera(int id){
+        String sql = "SELECT * FROM tessera WHERE id = " + id;
         ResultSet rs = Query.doQueryRS(sql);
         if(rs != null){
             try {
@@ -23,5 +26,21 @@ public class StoreTessera{
                 }
             }
         return null;
+    }
+    
+    public static ArrayList<Tessera> getTesseraFromContratto(int idContratto){
+    	String sql = "SELECT * FROM tessera WHERE id_contratto = " + idContratto;
+    	ResultSet rs = Query.doQueryRS(sql);
+    	if(rs != null){
+			try {
+				ArrayList<Tessera> listaTessere = new ArrayList<Tessera>();
+				while(rs.next()){
+					listaTessere.add(new Tessera(rs.getInt("id"), rs.getInt("id_contratto")));
+				}
+				return listaTessere;
+			}catch (SQLException e){
+			}
+		}
+		return null;
     }
 }
