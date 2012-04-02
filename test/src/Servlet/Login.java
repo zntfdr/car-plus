acb8016405275d29d6bbf399e7c68d41bce37de6
Francesco Agnolazza <crash.pc89@gmail.com>
2012-04-02 16:35:50 +0000

    Sistemato gesione tipo utente

--- test/src/Servlet/Login.java
+++ test/src/Servlet/Login.java
@@ -2,10 +2,14 @@ package Servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.util.ArrayList;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+
+import Entita.Contratto;
 import Entita.Utente;
+import Entita.Utente.Type;
 import Store.*;
 import Utils.*;
 
@@ -29,14 +33,25 @@ public class Login extends HttpServlet {
 			page = "html/login.html";
 		}
 		else if(login.getPassword().compareTo(password) == 0) {
+			if (StoreCliente_business.readCliente_business(email) != null){
+				login.setUserType(Type.BUSINESS);
+				ArrayList<Contratto> listaContratti = StoreContratto.listaContrattiUtente(email);
+				session.setAttribute("listaContratti",listaContratti);
+			}
+			else if(StoreCliente.readCliente(email) != null){
+				login.setUserType(Type.CLIENTE);
+				ArrayList<Contratto> listaContratti = StoreContratto.listaContrattiUtente(email);
+				session.setAttribute("listaContratti",listaContratti);
+			}
+			else{
+				login.setUserType(Type.USER);
+			}
 			session.setAttribute("utente_loggato",login);
-			page = "jsp/user_home.jsp"; //qui inviamo anche i dati session.setAttribute("utente",login);
+			page = "jsp/user_home.jsp";
 		}
 		else{
 			page = "html/login.html?fail=TRUE";	
 		}            
 		res.sendRedirect(page);
-		//RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(page);
-		//dispatcher.forward(req, res);
 	}
 }
