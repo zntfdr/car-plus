ac11afec9569b7863f18beb072042af8750c9371
Nicola Comand <nicmen89@gmail.com>
2012-04-02 11:50:21 +0000

    nuovo

---
+++ test/src/Store/StoreDisponibilita.java
@@ -0,0 +1,34 @@
+package Store;
+import java.sql.*;
+import java.util.ArrayList;
+
+import Entita.Abbonamento;
+import Entita.Disponibilita;
+import Entita.Tragitto_CP;
+import Utils.*;
+
+public class StoreDisponibilita{	
+	
+	public static void insertDisponibilita(Disponibilita dis){
+		String sql = "INSERT INTO disponibilita(modello_macchina, nome_abbonamento) VALUES('" + dis.getModello() + "', '" + dis.getNome_abb() +"')";
+	  
+		Query.doQuery(sql);
+		return;
+	}
+
+	public static  Disponibilita readDisponibilita(String nome){
+		String sql = "SELECT * FROM disponibilita WHERE nome_abbonamento = '" + nome + "'";
+		ResultSet rs = Query.doQueryRS(sql);
+		if(rs != null){
+			try {
+				while(rs.next()){
+					return new Disponibilita(rs.getInt("modello_macchina"), rs.getInt("nome_abbonamento"));
+				}
+			}catch (SQLException e){
+			}
+		}
+		return null;
+	}
+	
+
+}
