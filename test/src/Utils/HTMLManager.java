b0847e3354c51b7439e637356f569500b42a3f41
Gianmarco Nalin <gianma.nalin89@gmail.com>
2012-04-05 09:15:48 +0000

    Classe per la generazione di codice html comune a più pagine

---
+++ test/src/Utils/HTMLManager.java
@@ -0,0 +1,27 @@
+package Utils;
+import javax.servlet.http.HttpSession;
+import Entita.Utente;
+
+public class HTMLManager {
+	
+	public static String getHeader(HttpSession session) {
+		Utente user = (Utente) session.getAttribute("utente_loggato");
+		String header = "<div id=\"header\">" +
+				"<ul>" +
+				"<li><a href=\"../html/index.html\"><img src=\"../img/logo.png\" alt=\"Logo\"/></a></li>" +
+				"<li><a href=\"../html/index.html\">Home</a></li>" +
+				"<li><a href=\"#\">Azienda</a></li>" +
+				"<li><a href=\"#\">Servizi</a></li>" +
+				"<li> <a href=\"#\">Contattaci</a></li>";
+
+		if (user == null) {
+			header += "<li><a href=\"html/login.html\">Login</a></li>";
+		} else {
+			header += "<li><a href=\"user_home.jsp\">MyPage+</a></li><li><a href=\"logout.jsp\">Logout</a></li>";
+		}
+		
+		header += "</ul></div>";
+		
+		return header;
+	}
+}
