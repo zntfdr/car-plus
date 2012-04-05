febcd23d7ed6b35f354eaa31b2f83fb66794a5a6
Gianmarco Nalin <gianma.nalin89@gmail.com>
2012-04-05 10:27:32 +0000

    Modificata pagina login redirect

--- test/src/Utils/HTMLManager.java
+++ test/src/Utils/HTMLManager.java
@@ -8,14 +8,14 @@ public class HTMLManager {
 		Utente user = (Utente) session.getAttribute("utente_loggato");
 		String header = "<div id=\"header\">" +
 				"<ul>" +
-				"<li><a href=\"../html/index.html\"><img src=\"../img/logo.png\" alt=\"Logo\"/></a></li>" +
-				"<li><a href=\"../html/index.html\">Home</a></li>" +
+				"<li><a href=\".\"><img src=\"../img/logo.png\" alt=\"Logo\"/></a></li>" +
+				"<li><a href=\".\">Home</a></li>" +
 				"<li><a href=\"#\">Azienda</a></li>" +
 				"<li><a href=\"#\">Servizi</a></li>" +
 				"<li> <a href=\"#\">Contattaci</a></li>";
 
 		if (user == null) {
-			header += "<li><a href=\"html/login.html\">Login</a></li>";
+			header += "<li><a href=\"jsp/login.jsp\">Login</a></li>";
 		} else {
 			header += "<li><a href=\"user_home.jsp\">MyPage+</a></li><li><a href=\"logout.jsp\">Logout</a></li>";
 		}
