14e7502e2d6245d27471dc109f5a9a7e77c96c75
Francesco Agnolazza <crash.pc89@gmail.com>
2012-04-05 08:07:41 +0000

    Completata pagina jsp

--- test/src/Servlet/Inserimento_tragitto_cs.java
+++ test/src/Servlet/Inserimento_tragitto_cs.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -15,30 +16,17 @@ public class Inserimento_tragitto_cs extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
-		Contratto contratto = StoreContratto.readContratto(Integer.parseInt(req.getParameter("contratto")));
-		Tessera tessera = StoreTessera.readTessera(Integer.parseInt(req.getParameter("tessera")));
-		String tempoPrelievo = req.getParameter("tempoPrelievo");
-		String tempoConsegna = req.getParameter("tempoConesegna");
-		//Se tutto va bene, comincio a definire la sessione
+		
 		HttpSession session = req.getSession();
 		
-		//Tragitto_CS tragittoCS = new Tragitto_CS(0,tessera.getId(),targa, tempoPrelievo, tempoConsegna);
+		Calendar tempoPrelievo = Utils.TimeString.parseSQLTimestampToCalendar((String)session.getAttribute("tempoPrelievo"));
+		Calendar tempoConsegna = Utils.TimeString.parseSQLTimestampToCalendar((String)session.getAttribute("tempoConsegna"));
 		
-		String sql = "SELECT MCS.targa FROM macchina_cs AS MCS INNER JOIN disponibilita AS D ON MCS.id_modello = D.modello_macchina INNER JOIN tragitto_cs AS TCS ON MCS.targa = TCS.targa_cs  WHERE MCS.provincia = '" + req.getParameter("provincia_stazione") + "' AND MCS.citta = '" + req.getParameter("citta_stazione") + "' AND MCS.prenotabile = 'true' AND D.nome_abbonamento = '"+ contratto.getNome_abbonamento()+"' AND TCS.tempo_consegna < '"+ tempoPrelievo +"'";
-		ResultSet rs = Utils.Query.doQueryRS(sql);
-		if(rs != null){
-			try {
-				ArrayList<String> listaTarghe = new ArrayList<String>();
-				while(rs.next()){
-					listaTarghe.add(rs.getString("targa"));
-				}
-				ArrayList<Macchina_CS> listaMacchine = StoreMacchina_CS.readMacchina_CS_List(listaTarghe);
-				session.setAttribute("listaStazioni", listaMacchine);
-			} catch(SQLException e){
-				
-			}
-		}
-		String page = "jsp/new_tragitto_cs.jsp";
+		Tragitto_CS tragitto = new Tragitto_CS(0, Integer.parseInt(req.getParameter("idTessera")), req.getParameter("carplate"),tempoPrelievo, tempoConsegna,null,null, 0, false);
+		StoreTragitto_CS.insertTragitto_CS(tragitto);
+		
+		//Se tutto va bene, comincio a definire la sessione				
+		String page = "jsp/user_home.jsp";
 		res.sendRedirect(page);
 	}
 }
