eb84e20af780323756d342448ae83a359dbb5175
Nicola Comand <nicmen89@gmail.com>
2012-04-06 10:48:16 +0000

    trova i tragitti associati ad un contratto

---
+++ test/src/Servlet/Vista_tragitti_contratto.java
@@ -0,0 +1,44 @@
+package Servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+import java.util.ArrayList;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import Entita.*;
+import Utils.*;
+import Store.*;
+
+/**
+ *
+ */
+public class Vista_tragitti_contratto extends HttpServlet {
+
+	/**
+	 * Gestisce le richieste HTTP GETT
+	 * 
+	 * @param req
+	 *            la richiesta ricevuta dal client.
+	 * @param res
+	 *            la rispota fornita dal server.
+	 * 
+	 * @throws ServletException
+	 *             se ci sono problemi nell'esecuzione della servlet.
+	 * @throws IOException
+	 *             se ci sono problemi nella comunicazione client-server.
+	 */
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		String page = "";
+		int contratto_ID = Integer.parseInt(req.getParameter("id"));
+		ArrayList<Tragitto_CS_info> lista = Store.StoreTragitto_CS_info.readTragitto_CS_info_contratto(contratto_ID);
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("lista_tragitti", lista); //passo il costrutto parametri alla pagina
+		page = "";
+			
+		res.sendRedirect(page);
+	}
+}
