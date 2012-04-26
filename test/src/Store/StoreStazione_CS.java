8a23b1c68ff3b4c5b698abe78ef5669ab734a3ad
Gianmarco Nalin <gianma.nalin89@gmail.com>
2012-04-26 20:50:21 +0000

    Aggiunti metodi per update_cs_car.jsp

--- test/src/Store/StoreStazione_CS.java
+++ test/src/Store/StoreStazione_CS.java
@@ -72,7 +72,34 @@ public class StoreStazione_CS{
 		}
 		return null;
 	}
-
+	
+	public static ArrayList<String> getCittaStazioniCS(String provincia){
+		String sql = "SELECT DISTINCT citta FROM stazione_CS WHERE provincia = '" + provincia + "'";
+		ResultSet rs = null;
+		try {
+			rs = Query.doQueryRS(sql);
+			if(rs != null){
+				ArrayList<String> list = new ArrayList<String>();
+				while(rs.next()){
+					list.add(rs.getString("citta"));
+				}
+				return list;
+			}
+		}catch (SQLException e){
+		}
+		finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+			} finally {
+				rs = null;
+			}
+		}
+		return null;
+	}
+	
 	public static ArrayList<Stazione_CS> readStazione_CS_List(ArrayList<String> listaStazioni){
 		String sql = "SELECT * FROM stazione_cs WHERE ";
 		for (int i=0; i<listaStazioni.size() - 1; i++){
@@ -103,7 +130,34 @@ public class StoreStazione_CS{
 		}
 		return null;
 	}
-
+	
+	public static ArrayList<Stazione_CS> getStazioniCSPerCittaEProvincia(String provincia, String citta){
+		String sql = "SELECT * FROM stazione_cs WHERE provincia = '" + provincia + "' AND citta = '" + citta + "'";
+		ResultSet rs = null;
+		try {
+			rs = Query.doQueryRS(sql);
+			if(rs != null){
+				ArrayList<Stazione_CS> Stazioni_List = new ArrayList<Stazione_CS>();
+				while(rs.next()){
+					Stazioni_List.add(new Stazione_CS(rs.getString("citta"), rs.getString("provincia"), rs.getString("nome"),rs.getString("indirizzo"),rs.getInt("num_posti")));
+				}
+				return Stazioni_List;
+			}
+		}catch (SQLException e){
+		}
+		finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+			} finally {
+				rs = null;
+			}
+		}
+		return null;
+	}
+	
 	public static ArrayList<Stazione_CS> getStazione_CS() {
 		String sql_query = "SELECT * FROM stazione_cs ORDER BY provincia, citta, nome";
 		ResultSet rs = null;
